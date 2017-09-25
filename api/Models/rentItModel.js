97a19b11768d5d540e3531268be565948c1ecc1c
K-12 <grink.lukas@gmail.com>
2017-09-25 14:09:56 +0300

    DB Model complete

--- api/Models/rentItModel.js
+++ api/Models/rentItModel.js
@@ -27,12 +27,41 @@ var AdModel = new Model({
     default: ['available']
   },
   cost: {
-    type: Number,
-    required: 'Enter the name of the item'
+    type: Number
   },
-  owner_name: {
-    type: String
+  user_ID: {
+    type: String,
+      required: 'Enter the owner ID'
   }
 });
 
+var UserModel = new Model({
+    ID:{
+      type: String,
+        required: 'Enter user ID'
+    },
+    name: {
+      type: String,
+        required: 'Enter user name'
+    },
+    description: {
+      type: String
+    },
+    number: {
+      type: String,
+        required: 'Enter user phone number'
+    },
+    location:{
+      type: String,
+        enum: ['Vilnius', 'Kaunas', 'Klaipeda', 'Kitas'],
+        default: ['Kitas']
+    },
+    registered_date: {
+      type: date,
+        default date.now()
+    }
+
+});
+
 module.exports = mongoose.model('Ads', AdModel);
+module.exports = mongoose.model('Users', UserModel);
